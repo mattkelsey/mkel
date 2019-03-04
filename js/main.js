f1b2d4f31059470208d301a0cd65ab21b24242d2
Matt Kelsey <mattkelseymk@gmail.com>
2019-03-04 01:20:02 -0500

    Add particle system and mkel diagram

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles', 'particles.json', function() {
-  console.log('callback - particles.js config loaded');
+particlesJS.load('particles', 'js/particles.json', function() {
+  console.log('callback - partiules.js config loaded');
 });
