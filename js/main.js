5e1e1635fdfbc974f4dabcca9cb233748757bd2a
Matt Kelsey <mattkelseymk@gmail.com>
2019-03-03 23:02:50 -0500

    update particle config filepath

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-particlesJS.load('particles', 'assets/particles.json', function() {
+particlesJS.load('particles', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
